02fddb8951d07de1488f0be150f16a205d6a07a2
Jinhao Zhou <j5zhou@ucsd.edu>
2022-02-08 20:11:00 -0800

    jquery version

--- EventListApp/index.js
+++ EventListApp/index.js
@@ -3,36 +3,29 @@ const Appapi = (() => {
     path = "events";
 
     const getEvents = () =>
-        fetch([baseurl, path].join("/")).then((response) => response.json());
+        //fetch([baseurl, path].join("/")).then((response) => response.json());
+        $.ajax({
+            url: [baseurl, path].join("/"),
+        });
 
-    const addEvents = (events) =>
-        fetch([baseurl, path].join("/"), {
-            method: "POST",
-            body: JSON.stringify(events),
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-        }).then((response) => response.json());
+    const addEvents = (event) =>
+        $.ajax({
+            type: "POST",
+            url: [baseurl, path].join("/"),
+            data: event,
+        });
+    const deleteEvents = id =>
+        $.ajax({
+            type: "DELETE",
+            url: [baseurl, path, id].join("/"),
+        });
 
-    const deleteEvents = (id) =>
-        fetch([baseurl, path, id].join("/"), {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-        }).then((response) => response.json());;
-
-    const updateEvents = (id, events) =>
-        fetch([baseurl, path, id].join("/"), {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(events),
-        }).then((response) => response.json());;
-
+    const updateEvents = (id, event) =>
+        $.ajax({
+            type: "PUT",
+            url: [baseurl, path, id].join("/"),
+            data: event
+        });
 
     return {
         deleteEvents,
@@ -63,7 +56,7 @@ const View = (() => {
         if (M < 10) {
             M = "0" + M;
         }
-        let D = parseInt(date.getDate());
+        let D = parseInt(date.getDate())+1;
         if (D < 10) {
             D = "0" + D;
         }
@@ -72,15 +65,16 @@ const View = (() => {
 
     const createTmp = (arr) => {
         let tmp = "";
+
         arr.forEach((ele) => {
             let startdate = new Date(+ele.startDate);
             let enddate = new Date(+ele.endDate);
-            console.log(formatDate(startdate));
+            //console.log(formatDate(startdate));
             tmp += `
                 <li class="flex-containter" id="${ele.id}">
-                    <input class="event-item event-input-box" name="eventName" disabled value="${ele.eventName}">
-                    <input class="event-item event-input-box" name="startDate" disabled value="${formatDate(startdate)}">
-                    <input class="event-item event-input-box" name="endDate"  disabled value="${formatDate(enddate)}">
+                    <input class="event-item event-input-box name-input" name="eventName" disabled value="${ele.eventName}">
+                    <input class="event-item event-input-box startDate-input" type="date" name="startDate" disabled value="${formatDate(startdate)}">
+                    <input class="event-item event-input-box endDate-input" type="date" name="endDate"  disabled value="${formatDate(enddate)}">
                     <div class="event-btn-group bt-group-1 show">
                     <button class="event-btns edit_btn" type="button">Edit</button>
                     <button class="event-btns delete_btn" type="button">Delete</button>
@@ -94,9 +88,9 @@ const View = (() => {
         });
         tmp += `
         <li class="flex-containter hidden" id="add_field">
-        <input class="event-item event-input-box" name="eventName" id="addfield-name">
-        <input class="event-item event-input-box" name="startDate" id="addfield-startDate">
-        <input class="event-item event-input-box" name="endDate" id="addfield-endDate">
+        <input class="event-item event-input-box"  name="eventName" id="addfield-name">
+        <input class="event-item event-input-box" type="date" name="startDate" id="addfield-startDate">
+        <input class="event-item event-input-box" type="date" name="endDate" id="addfield-endDate">
         <div class="event-btn-group show">
         <button class="event-btns" type="button" id="addfield-savebtn" >SAVE</button>
         <button class="event-btns" type="button" id="addfield-closebtn">CLOSE</button>
@@ -116,28 +110,17 @@ const View = (() => {
 //------------------------------- Model---------------------------------------- 
 const Model = ((api, view) => {
     class Events {
-        constructor(name,start_date, end_date) {
-            let startArr = start_date.split("-");
-            let endArr = end_date.split("-");
-            let startD = new Date();
-            startD.setDate(parseInt(startArr[2]));
-            startD.setMonth(parseInt(startArr[1])-1);
-            startD.setFullYear(parseInt(startArr[0]));
-            let endD = new Date();
-            endD.setDate(parseInt(endArr[2]));
-            endD.setMonth(parseInt(endArr[1])-1);
-            endD.setFullYear(parseInt(endArr[0]));
-
-            this.startDate = startD.getTime();
-            this.endDate = endD.getTime();
+        constructor(name, start_date, end_date) {
+            this.startDate = start_date;
+            this.endDate = end_date;
             this.eventName = name;
-
         }
     }
 
     class State {
         #eventslist = [];
         #AddField = false;
+        #pageSize = 2;
 
         get eventslist() {
             return this.#eventslist;
@@ -156,60 +139,40 @@ const Model = ((api, view) => {
             // render the todolist
             const ele = document.querySelector(view.domstr.eventslist);
             const tmp = view.createTmp(this.#eventslist);
-            //console.log(tmp);
-            //console.log(ele);
             view.render(ele, tmp);
 
             //bind addfield close btn
-            const closeBtn = document.querySelector(view.domstr.addfield_closeBtn);
-            closeBtn.addEventListener("click", (event) => {
+            $(view.domstr.addfield_closeBtn).bind("click", (event) => {
                 event.preventDefault();
                 this.#AddField = false;
-                const addFiled = document.querySelector(view.domstr.addFiled);
-                addFiled.classList.add("hidden");
+                $(view.domstr.addFiled).addClass("hidden");
             });
 
+
             //bind addfield save btn
-            const saveBtn = document.querySelector(view.domstr.addfield_saveBtn);
-            saveBtn.addEventListener("click", (event) => {
+            $(view.domstr.addfield_saveBtn).bind("click", (event) => {
                 event.preventDefault();
                 this.#AddField = false;
                 const addFiled = document.querySelector(view.domstr.addFiled);
                 addFiled.classList.add("hidden");
 
                 //get the data:
-                let name = document.querySelector(view.domstr.addfield_name);
-                let endDate = document.querySelector(view.domstr.addfield_endDate);
-                let startDate = document.querySelector(view.domstr.addfield_startDate);
+                let name = $(view.domstr.addfield_name).val();
+                let endDate = document.querySelector(view.domstr.addfield_endDate).valueAsNumber;
+                let startDate = document.querySelector(view.domstr.addfield_startDate).valueAsNumber;
+                const new_event = new Events(name, startDate, endDate);
 
-                const new_event = new Events(name.value, startDate.value, endDate.value);
-                
-                
+
                 api.addEvents(new_event).then((newEventlists) => {
                     this.#eventslist = [newEventlists, ...this.#eventslist];
                     console.log(this.#eventslist);
-                    name.value="";
-                    endDate.value="";
-                    startDate.value="";
+                    name.value = "";
+                    endDate.value = "";
+                    startDate.value = "";
                 });
-                
 
             });
 
-            /*
-            //bind the deleteBtn
-            const deleteBtn = document.querySelector(view.domstr.deletebtn);
-            deleteBtn.addEventListener("click", (event) => {
-                const deleteid = parseInt(event.target.parentNode.parentNode.id);
-                console.log("deleteid:",deleteid);
-                console.log(event.target.parentNode.parentNode);
-                this.#eventslist = this.#eventslist.filter((ele) => {
-                    return +ele.id !== deleteid;
-                });
-                console.log(this.#eventslist);
-                //api.deleteEvents(deleteid);
-            });
-            */
         }
     }
 
@@ -233,24 +196,22 @@ const Controller = ((model, view) => {
     const state = new model.State();
 
     const addEvents = () => {
-        const addBtn = document.querySelector(view.domstr.addBtn);
+        //const addBtn = $(view.domstr.addBtn);
 
-        addBtn.addEventListener("click", (event) => {
+        $(view.domstr.addBtn).bind("click", (event) => {
+            event.preventDefault();
             if (!state.AddField) {
                 state.AddField = true;
-                const addFiled = document.querySelector(view.domstr.addFiled);
-                addFiled.classList.remove("hidden");
+                $(view.domstr.addFiled).removeClass("hidden");
             }
         });
     };
 
     const BtnEvents = () => {
-        const ele = document.querySelector(view.domstr.eventslist);
-        ele.addEventListener("click", (event) => {
-            event.preventDefault();
+        $(view.domstr.eventslist).bind("click", (event) => {
             const nodeId = parseInt(event.target.parentNode.parentNode.id);
             //delete events
-            if(event.target.classList.contains("delete_btn")){
+            if (event.target.classList.contains("delete_btn")) {
                 if (nodeId !== '') {
                     state.eventslist = state.eventslist.filter((ev) => {
                         return +ev.id !== +nodeId;
@@ -259,71 +220,57 @@ const Controller = ((model, view) => {
                 }
             }
             //edit events
-            if(event.target.classList.contains("edit_btn")){
+            if (event.target.classList.contains("edit_btn")) {
                 if (nodeId !== '') {
                     //switch the button groups:
                     const node = event.target.parentNode.parentNode;
-                    const btn_group1 = event.target.parentNode;
-                    const btn_group2 = (node.childNodes)[9];
-                    console.log(node.childNodes);
-                    btn_group2.classList.add("show");
-                    btn_group2.classList.remove("hidden");
-                    btn_group1.classList.add("hidden");
-                    btn_group1.classList.remove("show");
+                    const btn_group1 = $(`#${nodeId} .bt-group-1`);
+                    const btn_group2 = $(`#${nodeId} .bt-group-2`);
+                    btn_group2.addClass("show");
+                    btn_group2.removeClass("hidden");
+                    btn_group1.addClass("hidden");
+                    btn_group1.removeClass("show");
 
                     //let inputbox enable
-                    const nameInput = (node.childNodes)[1];
-                    const startDateInput = (node.childNodes)[3];
-                    const endDateInput = (node.childNodes)[5];
+                    const nameInput = $(`#${nodeId} .name-input`)[0];
+                    console.log(nameInput);
+                    const startDateInput = $(`#${nodeId} .startDate-input`)[0];
+                    const endDateInput = $(`#${nodeId} .endDate-input`)[0];
 
                     nameInput.disabled = false;
                     startDateInput.disabled = false;
                     endDateInput.disabled = false;
                 }
             }
-
+            
             //save events
-            if(event.target.classList.contains("save_btn")){
+            if (event.target.classList.contains("save_btn")) {
                 if (nodeId !== '') {
                     //get the data:
-                    const node = event.target.parentNode.parentNode;
-                    const nameInput = (node.childNodes)[1];
-                    const startDateInput = (node.childNodes)[3];
-                    const endDateInput = (node.childNodes)[5];
-                    /*
-                    state.eventslist.forEach((ele)=>{
-                        if(+ele.id===nodeId){
-                            console.log(ele);
-                            ele.eventName=nameInput.value;
-                            ele.startDate
-                            ele
-                        }
-                    })
-                    */
-                    let edit_event = new model.Events(nameInput.value, startDateInput.value, endDateInput.value); 
-                               
-                    model.updateEvents(nodeId,edit_event);
-                    
-                    }
+                    let name = $(`#${nodeId} .name-input`).val();
+                    let startDate = $(`#${nodeId} .startDate-input`)[0].valueAsNumber;
+                    let endDate = $(`#${nodeId} .endDate-input`)[0].valueAsNumber;
+                    let edit_event = new model.Events(name, startDate, endDate);
+                    model.updateEvents(nodeId, edit_event);
+                }
 
             }
+
             //Close events:
-            if(event.target.classList.contains("close_btn")){
+            if (event.target.classList.contains("close_btn")) {
                 if (nodeId !== '') {
                     //switch the button groups:
-                    const node = event.target.parentNode.parentNode;
-                    const btn_group2 = event.target.parentNode;
-                    const btn_group1 = (node.childNodes)[7];
-                    console.log(btn_group1);
-                    btn_group2.classList.add("hidden");
-                    btn_group2.classList.remove("show");
-                    btn_group1.classList.add("show");
-                    btn_group1.classList.remove("hidden");
+                    const btn_group1 = $(`#${nodeId} .bt-group-1`);
+                    const btn_group2 = $(`#${nodeId} .bt-group-2`);
+                    btn_group1.addClass("show");
+                    btn_group1.removeClass("hidden");
+                    btn_group2.addClass("hidden");
+                    btn_group2.removeClass("show");
 
                     //let inputbox enable
-                    const nameInput = (node.childNodes)[1];
-                    const startDateInput = (node.childNodes)[3];
-                    const endDateInput = (node.childNodes)[5];
+                    const nameInput = $(`#${nodeId} .name-input`)[0];
+                    const startDateInput = $(`#${nodeId} .startDate-input`)[0];
+                    const endDateInput = $(`#${nodeId} .endDate-input`)[0];
 
                     nameInput.disabled = true;
                     startDateInput.disabled = true;
